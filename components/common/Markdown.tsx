b3d5c58b3899b96100fe5c5889e37a751493b566
sunhx <673686754@qq.com>
2023-10-21 12:40:36 +0800

    feat: Markdown component

---
+++ components/common/Markdown.tsx
@@ -0,0 +1,38 @@
+import { memo } from 'react'
+import ReactMarkdown, { Options } from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import remarkGfm from 'remark-gfm'
+
+function Markdown({ children, className = '', ...props }: Options) {
+  return (
+    <ReactMarkdown
+      components={{
+        code(props) {
+          const { children, className, node, ...rest } = props
+          const match = /language-(\w+)/.exec(className || '')
+          return match ? (
+            <SyntaxHighlighter
+              {...rest}
+              children={String(children).replace(/\n$/, '')}
+              style={a11yDark}
+              language={match?.[1]}
+              PreTag="div"
+            />
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          )
+        }
+      }}
+      remarkPlugins={[remarkGfm]}
+      className={`markdown prose dark:prose-invert ${className}`}
+      {...props}
+    >
+      {children}
+    </ReactMarkdown>
+  )
+}
+
+export default memo(Markdown)
