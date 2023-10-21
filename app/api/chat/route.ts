5272183e739527ce260f2beb09d4a801727513b6
sunhx <673686754@qq.com>
2023-10-21 21:32:27 +0800

    feat: chat api

---
+++ app/api/chat/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest } from 'next/server'
+
+import { sleep } from '@/common/util'
+import { MessageRequestBody } from '@/types/chat'
+
+export async function POST(request: NextRequest) {
+  const { messages } = (await request.json()) as MessageRequestBody
+  const encoder = new TextEncoder()
+  const stream = new ReadableStream({
+    async start(controller) {
+      const messageText = messages[messages.length - 1].content
+      for (let i = 0; i < messageText.length; i++) {
+        await sleep(100)
+        controller.enqueue(encoder.encode(messageText[i]))
+      }
+      controller.close()
+    }
+  })
+  return new Response(stream)
+}
