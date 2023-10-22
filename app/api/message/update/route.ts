f539b9271940d71b4311cd239215cd1d7d208e7f
sunhx <673686754@qq.com>
2023-10-22 14:48:06 +0800

    feat: message update api

---
+++ app/api/message/update/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import prisma from '@/lib/prisma'
+
+export async function POST(request: NextRequest) {
+  const body = await request.json()
+  const { id, ...data } = body
+  if (!data.chatId) {
+    const chat = await prisma.chat.create({
+      data: {
+        title: '新对话'
+      }
+    })
+    data.chatId = chat.id
+  }
+  const message = await prisma.message.upsert({
+    create: data,
+    update: data,
+    where: {
+      id
+    }
+  })
+  return NextResponse.json({ code: 0, data: { message } })
+}
