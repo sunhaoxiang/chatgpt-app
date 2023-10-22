636f9d63eac580ed75442c32aa689092291c4cf5
sunhx <673686754@qq.com>
2023-10-22 17:04:56 +0800

    feat: chatList api

---
+++ app/api/chat/list/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import prisma from '@/lib/prisma'
+
+export async function GET(request: NextRequest) {
+  const param = request.nextUrl.searchParams.get('page')
+  const page = param ? parseInt(param) : 1
+  const list = await prisma.chat.findMany({
+    skip: (page - 1) * 20,
+    take: 20,
+    orderBy: {
+      updateTime: 'desc'
+    }
+  })
+  return NextResponse.json({ code: 0, data: { list } })
+}
