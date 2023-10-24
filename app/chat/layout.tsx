502ff240f77de3a82e530391229c5f106eab8369
sunhx <673686754@qq.com>
2023-10-24 08:42:36 +0800

    feat: delete useless files

--- app/chat/layout.tsx
+++
@@ -1,5 +0,0 @@
-import type { ReactNode } from 'react'
-
-export default function RootLayout({ children }: { children: ReactNode }) {
-  return <div className="bg-orange-500 p-10">{children}</div>
-}
